03572ed150cee7b92d4f772b081be8b35021b5e7
Hyuni Kim <spaceship00@naver.com>
2024-03-10 07:32:30 +0000

    Control max iterations

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import mouseDragInteraction from './modules/interactions/mouseDragInteraction.js';
 import mouseZoomInteraction from './modules/interactions/mouseZoomInteraction.js';
 
-const canvas = document.getElementById('myCanvas');
+const canvas = document.getElementById('fractalCanvas');
 
 mouseDragInteraction.register(canvas);
 mouseZoomInteraction.register(canvas);
---
+++ modules/maxIterations.js
@@ -0,0 +1,16 @@
+const sliderElement = document.getElementById('max-iterations-slider');
+const inputElement = document.getElementById('max-iterations');
+
+let maxIterations = 100;
+
+function onMaxIterationsUpdated(newMaxIterations) {
+  maxIterations = newMaxIterations;
+}
+
+sliderElement.addEventListener('input', event => {
+
+});
+
+inputElement.addEventListener('input', event => {
+
+});
