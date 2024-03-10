f7c1a240bc1b50e6828c714bcc3c9a5f2da5b06f
Hyuni Kim <spaceship00@naver.com>
2024-03-10 08:54:03 +0000

    Control max iterations

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import mandelbrot from './modules/fractals/mandelbrot.js';
 import mouseDragInteraction from './modules/interactions/mouseDragInteraction.js';
 import mouseZoomInteraction from './modules/interactions/mouseZoomInteraction.js';
 import { InteractiveCanvas } from './modules/InteractiveCanvas.js';
-import { attachMaxIterationsListener } from './modules/maxIterations.js';
+import { attachMaxIterationsListener, listenToMaxIterations } from './modules/maxIterations.js';
 
 const interactiveCanvas = new InteractiveCanvas(600, 400, mandelbrot);
 const canvas = document.getElementById('fractalCanvas');
@@ -37,3 +37,6 @@ function paint(result) {
 interactiveCanvas.listen(paint);
 
 attachMaxIterationsListener();
+listenToMaxIterations(() => {
+  interactiveCanvas.populate();
+});
