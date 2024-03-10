0b3e53782ecac52e58a67ecde7e206be7cddc4af
Hyuni Kim <spaceship00@naver.com>
2024-03-10 06:57:08 +0000

    Separate mandelbrot calculation

--- index.js
+++ index.js
@@ -3,22 +3,3 @@ import mouseDragInteraction from './modules/interactions/mouseDragInteraction.js
 const canvas = document.getElementById('myCanvas');
 
 mouseDragInteraction.register(canvas);
-
-function mandelbrot(x, y) {
-    let zr = 0;
-    let zi = 0;
-    let i = 0;
-
-    while (i < maxIterations) {
-        const zrtemp = zr * zr - zi * zi + x;
-        zi = 2 * zr * zi + y;
-        zr = zrtemp;
-
-        if (zr * zr + zi * zi > 4) {
-            return i;
-        }
-        i++;
-    }
-
-    return -1; // Inside the Mandelbrot set 
-}
